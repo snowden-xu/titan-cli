cd5758b744db9fd431d1f23c22af938a32f8d5f6
snowden.xu <jxxuxuefeng@qq.com>
2021-06-23 22:44:05 +0800

    feat: 增加项目初始化命令

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// 注册一个命令 titan init
+const argv = require(`process`).argv;
+const command = argv[2];
+// const options = argv.slice(3);
+// let [option, param] = options;
+// option = option.replace('--', '');
+const lib ={
+    init: function (){
+        console.log('坑爹王能不骚？')
+    }
+}
+
+if(command){
+    if(lib[command]){
+        lib[command]();
+    }else{
+        console.log('无效的命令')
+    }
+}else{
+    console.log('请输入命令')
+}
